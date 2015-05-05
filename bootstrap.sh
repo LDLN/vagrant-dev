0fb21c68b865a643336c4706d3bd7b0b7761c8c1
Nick Ihm <nickihm@Nicks-MacBook-Pro.local>
2015-05-04 23:02:06 -0400

    install mongo; setup dev

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
-#apt-get update
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
+
+apt-get update
 #apt-get upgrade -y
 
-sudo apt-get install -y gcc libc6-dev git mercurial bzr
+sudo apt-get install -y gcc libc6-dev git mercurial bzr mongodb-org
 
-if [ ! -d "/usr/local/go" ]; then
+#if [ ! -d "/usr/local/go" ]; then
     VERSION=1.4.2
     OS=linux
     ARCH=amd64
@@ -16,7 +19,7 @@ if [ ! -d "/usr/local/go" ]; then
     echo export PATH=\$PATH:/usr/local/go/bin >> .profile
     echo export GOPATH=\$HOME/go >> .profile
     echo export PATH=\$PATH:$GOPATH/bin >> .profile
-fi
+#fi
 
 mkdir /home/vagrant/go
 export GOPATH=/home/vagrant/go
@@ -24,17 +27,30 @@ export GOPATH=/home/vagrant/go
 /usr/local/go/bin/go get code.google.com/p/go.crypto/bcrypt
 /usr/local/go/bin/go get github.com/nu7hatch/gouuid
 /usr/local/go/bin/go get labix.org/v2/mgo
-/usr/local/go/bin/go get github.com/ldln/landline-basestation
 /usr/local/go/bin/go get code.google.com/p/go.net/websocket
 
-if [ ! -f "/etc/init.d/ldln_dev_startup.sh" ]; then
-    echo GOPATH=/home/vagrant/go >> /etc/init.d/ldln_dev_startup.sh
-    echo /home/vagrant/go/bin/revel run github.com/ldln/landline-basestation >> /etc/init.d/ldln_dev_startup.sh
-    echo /usr/local/go/bin/go run github.com/ldln/landline-basestation/ws/main.go >> /etc/init.d/ldln_dev_startup.sh
+# DEPLOY LDLN FOR PRODUCTION
+#/usr/local/go/bin/go get github.com/ldln/landline-basestation
+
+# DEPLOY LDLN FOR DEVELOPMENT
+git clone https://github.com/LDLN/landline-basestation.git /home/vagrant/landline-basestation
+mkdir -p $GOPATH/src/github.com/ldln/
+ln -s /home/vagrant/landline-basestation/ $GOPATH/src/github.com/ldln/landline-basestation
+
+mkdir $GOPATH/src/github.com/ldln/landline-basestation/app/tmp
+chown -R vagrant $GOPATH/*
+chown -R vagrant /home/vagrant/landline-basestation
+
+#if [ ! -f "/etc/init.d/ldln_dev_startup.sh" ]; then
+    echo "fuser -k 9000/tcp" >> /etc/init.d/ldln_dev_startup.sh
+    echo "fuser -k 8080/tcp" >> /etc/init.d/ldln_dev_startup.sh
+    echo "GOPATH=/home/vagrant/go" >> /etc/init.d/ldln_dev_startup.sh
+    echo "/home/vagrant/go/bin/revel run github.com/ldln/landline-basestation &" >> /etc/init.d/ldln_dev_startup.sh
+    echo "/usr/local/go/bin/go run /home/vagrant/go/src/github.com/ldln/landline-basestation/ws/main.go &" >> /etc/init.d/ldln_dev_startup.sh
 
     # start tilestream
     #/usr/bin/node /home/ubuntu/tilestream/index.js start --host 184.73.255.76 --tiles=/home/ubuntu/tilestream/tiles &
     
     chmod +x /etc/init.d/ldln_dev_startup.sh
     update-rc.d ldln_dev_startup.sh defaults
-fi
+#fi
